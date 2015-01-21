35ff9c29a27d9831ef763862bb242af30072691e
Aren Clegg <aclegg2011@gmail.com>
2015-01-20 16:03:41 -0800

    viper4android should go into priv-app
    Change-Id: I8cfd37b0852aabc335aa7d03bf55a551b66b50d3

--- device.mk
+++ device.mk
@@ -29,7 +29,7 @@ PRODUCT_COPY_FILES += \
 
 #Viper4Android
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/viper4android/viper4android.apk:system/app/Viper4Android/viper4android.apk \
+    $(LOCAL_PATH)/viper4android/viper4android.apk:system/priv-app/Viper4Android/viper4android.apk \
     $(LOCAL_PATH)/viper4android/lib/libv4a_fx_ics.so:system/lib/soundfx/libv4a_fx_ics.so 
 
 # Wifi
