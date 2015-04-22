0e3f939a2722887220723733356f0f17bb3810b5
Dan Pasanen <dan.pasanen@gmail.com>
2015-04-22 13:45:02 -0500

    thermal-engine: move to g3-common
    Change-Id: I36482f645c5c8c8f6cde96aebfa5b52736813ab5
    
    Conflicts:
    	device.mk

--- device.mk
+++ device.mk
@@ -27,10 +27,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
 
-# Thermal
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine-8974.conf:system/etc/thermal-engine-8974.conf
-
 # Select Tmobile LTE IPV6 on first boot
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/apn/apns-conf.xml:system/etc/apns-conf.xml
